8ed068a2525a77589ccd7bf29819a7d99e8ad3e1
Pranjal Varshney <92289608+VarshneyPranjal@users.noreply.github.com>
2026-06-20 18:53:58 +0530

    Initialize basic web server with health check

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func main() {
+	tmpl := template.Must(template.ParseFiles("templates/index.html"))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl.Execute(w, nil)
+	})
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Server is up and running"))
+	})
+
+	log.Println("Server running on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
