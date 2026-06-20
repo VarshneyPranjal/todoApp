fd4705fa886979ec7b7660000dfafa919b1073c9
Pranjal Varshney <92289608+VarshneyPranjal@users.noreply.github.com>
2026-06-20 21:01:53 +0530

    Add static file serving for '/static/' route

--- main.go
+++ main.go
@@ -9,6 +9,10 @@ import (
 func main() {
 	tmpl := template.Must(template.ParseFiles("templates/index.html"))
 
+	http.Handle("/static/",
+		http.StripPrefix("/static/",
+			http.FileServer(http.Dir("static"))))
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		tmpl.Execute(w, nil)
 	})
